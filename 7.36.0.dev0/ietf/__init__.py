3db51d0266c63a4b5845318f2f0c2b69890e4224
Kesara Rathnayake <kesara@fq.nz>
2021-09-02 15:22:36 +1200

    New branch for 7.36.0

---
+++ 7.36.0.dev0/ietf/__init__.py
@@ -0,0 +1,17 @@
+# Copyright The IETF Trust 2007-2020, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+
+from . import checks                           # pyflakes:ignore
+
+# Don't add patch number here:
+__version__ = "7.36.0"
+
+# set this to ".p1", ".p2", etc. after patching
+__patch__   = ""
+
+__date__    = "$Date: 2021-08-07 03:05:11 +1200 (Sat, 07 Aug 2021) $"
+
+__rev__     = "$Rev: 19289 $"
+
+__id__      = "$Id: __init__.py 19289 2021-08-06 15:05:11Z rjsparks@nostrum.com $"
