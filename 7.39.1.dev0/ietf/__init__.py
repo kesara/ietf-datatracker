ea0aad8b91aaa196b4aed74412316ebf8b0e0746
Kesara Rathnayake <kesara@fq.nz>
2021-10-28 04:42:48 +1300

    New development branch for 7.39.1.dev0

---
+++ 7.39.1.dev0/ietf/__init__.py
@@ -0,0 +1,17 @@
+# Copyright The IETF Trust 2007-2020, All Rights Reserved
+# -*- coding: utf-8 -*-
+
+
+from . import checks                           # pyflakes:ignore
+
+# Don't add patch number here:
+__version__ = "7.39.1.dev0"
+
+# set this to ".p1", ".p2", etc. after patching
+__patch__   = ""
+
+__date__    = "$Date: 2021-10-09 05:26:33 +1300 (Sat, 09 Oct 2021) $"
+
+__rev__     = "$Rev: 19408 $ (dev) Latest release: Rev. 19406 "
+
+__id__      = "$Id: __init__.py 19408 2021-10-08 16:26:33Z rjsparks@nostrum.com $"
